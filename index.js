adb8f8747d9b7f7ef92a078e4a6ade4f76262f64
Justin Verthein <Justinmackay88@hotmail.com>
2022-11-08 14:15:41 -1000

    commit tree 9a17c1e261d679aa0feb673f2efa095327d15ed7

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+      console.log(`Thank you, ${names[i]} for the wonder suprise gift!`);
+    }
+  
+    return names;
+  }
+  writeCards(names);
